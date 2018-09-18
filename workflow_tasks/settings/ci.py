30c2ca4c0815eb4aa0c4702ee8515505e2ce33c0
Ricardo Oyarzun <royarzun@gmail.com>
2018-09-18 16:49:03 -0300

    fix(settings): add db host to ci settings

--- workflow_tasks/settings/ci.py
+++ workflow_tasks/settings/ci.py
@@ -5,6 +5,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'travis_ci_test',
+        'HOST': 'localhost',
         'USER': 'postgres',
         'PORT': 5432,
     }
